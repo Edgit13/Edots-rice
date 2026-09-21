1facffa00ac38b07d516696be49f880635295769
Edgit13 <ed.koch13@gmail.com>
2026-09-21 22:20:35 +0300

    microfix

--- quickshell/bar/reload.sh
+++ quickshell/bar/reload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pkill -f 'qs .*bar/shell.qml' 2>/dev/null || true
+pkill -f 'qs .*bar/ShellNext.qml' 2>/dev/null || true
 qs -p ~/.config/quickshell/bar/ShellNext.qml>/dev/null 2>&1 &
